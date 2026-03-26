d0c60aa75f0e5f64d87a1631fac631699076e4f6
Sergio Navarro <nachonavarro197@gmail.com>
2026-03-26 00:33:53 -0300

    prueba de desbordamiento

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,7 +78,7 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
       </head>
-      <body className="font-sans antialiased">
+      <body className="font-sans antialiased overflow-x-hidden">
         {children}
         <Analytics />
       </body>
